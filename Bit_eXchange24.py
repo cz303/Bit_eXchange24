fe0e07b8ce732f023dfa1a3530497d22b088a9e0
Gaargod24 <54329985+Gaargod24@users.noreply.github.com>
2019-08-29 00:02:51 +0500

    Update Bit_eXchange24.py

--- Bit_eXchange24.py
+++ Bit_eXchange24.py
@@ -84,7 +84,7 @@ def next(message):
 		bot.send_message(message.chat.id,"Этот раздел в разработке. Скоро будет.")
 		second_menu(message)
 	elif message.text == '\U0001F4F2 Контакты':
-		bot.send_message(message.chat.id,"\U0001F4F2 Контакты: \n\n\U00002709 On-line поддержка - @Bit_ex24sup \n\U0001F52E *Новостной* канал - t.me/Bit_eX24", parse_mode= "Markdown")
+		bot.send_message(message.chat.id,"\U0001F4F2 Контакты: \n\n\U00002709 On-line поддержка - @Bit_ex24sup \n\U0001F52E <b>Новостной</b> канал - t.me/Bit_eX24", parse_mode= "HTML")
 		second_menu(message)
 	elif message.text == '\U0001F516 Активировать промокод':
 		msg = bot.send_message(message.chat.id, "Введите промокод:")
